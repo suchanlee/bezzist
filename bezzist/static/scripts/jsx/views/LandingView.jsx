054bcb1766bc6a09e9e1b7f174a7a334fc4a9334
Suchan Lee <suchanl@palantir.com>
2014-12-11 00:29:01 -0800

    added alert warning for safari private mode

--- bezzist/static/scripts/jsx/views/LandingView.jsx
+++ bezzist/static/scripts/jsx/views/LandingView.jsx
@@ -2,9 +2,16 @@
 'use strict';
 
 define(
-['react', 'components/question/QuestionBox', 'components/answer/UpcomingBox'],
-function (React, QuestionBox, UpcomingBox) {
+['react', 'components/question/QuestionBox', 'components/answer/UpcomingBox',
+ 'store'],
+function (React, QuestionBox, UpcomingBox, store) {
   return React.createClass({
+    componentDidMount: function() {
+      if (!store.enabled) {
+        alert("Bezzist does not work in Safari's Private Mode. We are sorry for the inconvinience.");
+      }
+    },
+
     render: function() {
       return (
         <div className='landing'>
