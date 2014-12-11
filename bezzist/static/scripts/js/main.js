d5ff20d307c240d833c67d7cb00b25595e006b0c
Suchan Lee <suchanl@palantir.com>
2014-12-11 01:06:21 -0800

    removed backbone dependency, switched to director for routing

--- bezzist/static/scripts/js/main.js
+++ bezzist/static/scripts/js/main.js
@@ -7,19 +7,16 @@ require.config({
     components: 'components',
     router: 'router',
     views: 'views',
+    director: '//rawgit.com/flatiron/director/master/build/director.min',
     jquery: '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min',
     underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min',
-    backbone: '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min',
     react: 'http://fb.me/react-with-addons-0.12.1.min',
     moment: '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.4/moment.min',
     store: '//cdnjs.cloudflare.com/ajax/libs/store.js/1.3.7/store.min'
   },
   shim: {
-    backbone: {
-      deps: [
-        'underscore',
-      ],
-      exports: 'Backbone'
+    director: {
+      exports: 'director'
     },
     moment: {
       exports: 'moment'
@@ -37,5 +34,6 @@ require.config({
 });
 
 require(['router/Router'], function(Router) {
-  var r = new Router();
+  var r = Router;
+  r.init();
 });
