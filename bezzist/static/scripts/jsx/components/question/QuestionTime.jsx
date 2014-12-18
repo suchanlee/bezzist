cb3dfac8876cd04b2526c88eb8c4a7eb85d29ecd
Suchan Lee <suchanl@palantir.com>
2014-12-17 21:42:55 -0800

    git st

--- bezzist/static/scripts/jsx/components/question/QuestionTime.jsx
+++ bezzist/static/scripts/jsx/components/question/QuestionTime.jsx
@@ -8,7 +8,7 @@ define(['react', 'moment'], function (React, moment) {
       var expr_dt, days;
       expr_dt = moment(this.props.q.created).add(7, 'days');
       days = moment.duration(expr_dt.diff(moment())).days() + 1;
-      if (days >= 0) {
+      if (days > 0) {
         return days;
       } else {
         return 0;
@@ -16,10 +16,15 @@ define(['react', 'moment'], function (React, moment) {
     },
 
     render: function() {
-      var rem_str;
+      var rem_str, dates;
 
       if (this.props.q) {
-        rem_str = this._getRemainingDays() + ' days left for this question';
+        dates = this._getRemainingDays();
+        if (dates > 0) {
+          rem_str = dates + ' days left for this question';
+        } else {
+          rem_str = 'This question is now closed.'
+        }
       } else {
         rem_str = '';
       }
