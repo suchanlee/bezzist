3f4f106289a71632a4e9a19b9809a2b1168e55a0
Suchan Lee <suchanl@palantir.com>
2015-05-13 22:51:28 -0700

    hyperlink links while escaping other bad things

--- bezzist/static/js/components/base/Row.react.js
+++ bezzist/static/js/components/base/Row.react.js
@@ -1,9 +1,15 @@
 /** @jsx React.DOM */
 'use strict';
 
+/*
+ * External libraries
+ */
 var React = require('react');
 var _ = require('underscore');
+var Autolinker = require('autolinker');
+
 var Utils = require('../../lib/Utils');
+
 
 var Row = React.createClass({
   shouldComponentUpdate: function(nextProps, nextState) {
@@ -13,7 +19,11 @@ var Row = React.createClass({
     return false;
   },
 
-  handleVoteClick: function(e) {
+  handleRowClick: function(e) {
+    // if link is clicked, return.
+    if (e.target.tagName === 'A') {
+      return;
+    }
     if (!this.props.hasVoted()) {
       this.props.vote();
     } else {
@@ -23,17 +33,25 @@ var Row = React.createClass({
     e.preventDefault();
   },
 
+  getContent: function() {
+    return Autolinker.link(Utils.sanitizeProfanity(this.props.content), {
+      newWindow: true,
+      stripPrefix: false,
+      phone: false,
+      hashtag: false,
+      truncate: 40
+    });
+  },
+
   render: function() {
     return (
       <li
         className={this.props.hasVoted() ? 'row-item voted' : 'row-item'}
-        onClick={this.handleVoteClick}>
+        onClick={this.handleRowClick}>
         <div className='row'>
           <div className='row-content'>
             <span className='row-idx'>{this.props.idx}</span>
-            <span className='row-text'>
-              {Utils.capitalize(Utils.sanitizeProfanity(this.props.content))}
-            </span>
+            <span className='row-text' dangerouslySetInnerHTML={{__html: this.getContent()}} />
           </div>
           <div className='vote-box'>
             <div className='vote-box-inner'>
