fe34c705a514c06c63a63ce743bc53770f807919
randytung <randytung8@hotmail.com>
2015-05-25 14:25:05 -0400

    deleted mail models and migrations

--- bezzist/mail/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Email(models.Model):
-    '''
-    Temporary model to hold emails until users are created
-    '''
-    email = models.EmailField(max_length=100)
